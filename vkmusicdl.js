473602c4d16e8dc9dc9bdacebf9a025452fb4521
Ken Lauguico <me@kenlauguico.com>
2013-10-03 00:34:12 -0700

    new bookmarklet

---
+++ vkmusicdl.js
@@ -0,0 +1,5 @@
+// Download Current Song - vk.com
+// by Ken Lauguico
+// 10/02/13
+
+javascript:var playnew = document.getElementsByClassName('play_new'); var mp3s = document.querySelectorAll("input[id^='audio_info']"); for(var i=0;i<playnew.length;i++) if (hasClass(playnew[i],'playing')) break; window.open(mp3s[i].value,'_blank'); 
