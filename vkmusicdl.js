82476d6a38ab3a98d4d1d21f9d0dedcdecfd278b
Ken Lauguico <me@kenlauguico.com>
2013-10-14 15:15:51 -0700

    minified

--- vkmusicdl.js
+++ vkmusicdl.js
@@ -4,5 +4,5 @@
 
 javascript:var playnew = document.getElementsByClassName('play_new'); var mp3s = document.querySelectorAll("input[id^='audio_info']"); for(var i=0;i<playnew.length;i++) if (hasClass(playnew[i],'playing')) break; window.open(mp3s[i].value.split(",")[0],'_blank');
 
-// Minified - http://javascript-minifier.com/
-javascript:for(var playnew=document.getElementsByClassName("play_new"),mp3s=document.querySelectorAll("input[id^='audio_info']"),i=0;i<playnew.length&&!hasClass(playnew[i],"playing");i++);window.open(mp3s[i].value.split(",")[0],"_blank");
+// Minified
+javascript:for(var a=document.getElementsByClassName("play_new"),b=document.querySelectorAll("input[id^='audio_info']"),c=0;c<a.length&&!hasClass(a[c],"playing");c++);window.open(b[c].value.split(",")[0],"_blank");
