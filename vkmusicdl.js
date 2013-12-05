53b567353c1a80528c9f1caf515f88b9c71ee671
Ken Lauguico <me@kenlauguico.com>
2013-12-05 08:57:30 -0800

    fixed comment typo

--- vkmusicdl.js
+++ vkmusicdl.js
@@ -4,5 +4,5 @@
 
 javascript:var playnew = document.getElementsByClassName('play_new'); var mp3s = document.querySelectorAll("input[id^='audio_info']"); for(var i=0;i<playnew.length;i++) if (hasClass(playnew[i],'playing')) break; window.open(mp3s[i].value.split(",")[0],'_blank');
 
-// Minified
+// Minified Version
 javascript:for(var a=document.getElementsByClassName("play_new"),b=document.querySelectorAll("input[id^='audio_info']"),c=0;c<a.length&&!hasClass(a[c],"playing");c++);window.open(b[c].value.split(",")[0],"_blank");
