9d134c7208f921f0ca0b2f4b727a4775a12853ee
Ken Lauguico <me@kenlauguico.com>
2013-12-05 09:12:48 -0800

    added comments to js

--- vkmusicdl.js
+++ vkmusicdl.js
@@ -2,7 +2,21 @@
 // by Ken Lauguico
 // 10/02/13
 
-javascript:var playnew = document.getElementsByClassName('play_new'); var mp3s = document.querySelectorAll("input[id^='audio_info']"); for(var i=0;i<playnew.length;i++) if (hasClass(playnew[i],'playing')) break; window.open(mp3s[i].value.split(",")[0],'_blank');
+javascript:
+
+// Collect array of all Playable Content
+var playnew = document.getElementsByClassName('play_new');
+
+// Collect array of all mp3 URLs reflecting the Playable Content
+var mp3s = document.querySelectorAll("input[id^='audio_info']");
+
+// Iterate through each Playable Content to see which is playing
+for (var i=0;i<playnew.length;i++)
+  if (hasClass(playnew[i],'playing')) break;
+
+// Get the value that matched which is playing and open that URL
+window.open(mp3s[i].value.split(",")[0],'_blank');
+
 
 // Minified Version
 javascript:for(var a=document.getElementsByClassName("play_new"),b=document.querySelectorAll("input[id^='audio_info']"),c=0;c<a.length&&!hasClass(a[c],"playing");c++);window.open(b[c].value.split(",")[0],"_blank");
